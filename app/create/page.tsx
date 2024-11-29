2f2929b2f41ef79db8e67cb4ed94fe070e51daf9
griebdaniel <griebdaniel94@gmail.com>
2024-11-29 17:20:17 +0200

    Reapply "removed important files needed for PR"
    This reverts commit a7d59b43246628d809bafd9bfe102dc6a514d185.

--- app/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import CreateShort from "@/app/components/create-short-form";
-
-export default function CreateShortPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center p-4">
-      <CreateShort />
-    </div>
-  );
-}
