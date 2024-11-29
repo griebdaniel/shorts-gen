9d5e689212e0884f43cdf6538324ca5f2f6667c5
griebdaniel <griebdaniel94@gmail.com>
2024-11-29 21:55:31 +0200

    for clean PR

--- app/actions/styles.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { db } from "@/app/db";
-import { styles } from "@/app/db/schema";
-
-export async function getStyles() {
-  return await db.select().from(styles);
-}
--- app/components/create-short-form/fields/video-kind-server.tsx
+++
@@ -1,8 +0,0 @@
-import { getVideoKinds } from "@/app/actions/video-kinds";
-import { VideoKindField } from "./video-kind";
-
-export async function VideoKindFieldServer() {
-  const videoKinds = await getVideoKinds();
-
-  return <VideoKindField videoKinds={videoKinds} />;
-}
